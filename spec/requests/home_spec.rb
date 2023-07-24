3d7e805048877834875c18eb66fd740e80e673fd
Olivier-Kango <olivierkango@gmail.com>
2023-07-24 18:19:56 +0300

    run rspec

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "succeeds" do
+      get root_path
+      expect(response).to be_successful
+    end
   end
 end
